76db873c42e2d1375b70623858bb4906815ce73c
jhon chaparro <wenikore@gmail.com>
2017-04-04 17:28:47 -0500

    it is a project with spring boot and gladle, it has configuration easy :)

---
+++ start-project-gradle/src/main/java/com/oreilly/boot/StartProjectGradleApplication.java
@@ -0,0 +1,12 @@
+package com.oreilly.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartProjectGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartProjectGradleApplication.class, args);
+	}
+}
